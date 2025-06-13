96e6573c79fdffbc9130de676bcec84a48db4595
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-13 20:50:02 +0700

    Use products.js file

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,4 +1,5 @@
 // Step One ==> Save the Data using Arrays and Objects
+/*
 const products = [{
   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs', 
@@ -23,7 +24,16 @@ const products = [{
     count: 56
   }, 
   priceCents: 799
+}, {
+  image: 'images/products/black-2-slot-toaster.jpg', 
+  name: '2 Slot Toaster - Black', 
+  rating: {
+    stars: 5,
+    count: 2197
+  }, 
+  priceCents: 1899
 }];
+*/
 
 let productsHTML = '';
 
