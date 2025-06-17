7cc4069b9ad067d364d49a32d1b8506695e39eda
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-17 17:39:55 +0700

    Generate HTML for the order summary

--- Amazon Project/data/cart.js
+++ Amazon Project/data/cart.js
@@ -1,5 +1,10 @@
-export const cart = []; // agar variablenya bisa dipake diluar file
-                        // cart.js
+export const cart = [{
+  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity: 2
+}, {
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity: 1
+}]; // agar variablenya bisa dipake diluar file cart.js
 
 export function addToCart(productId){
   let matchingItem;
