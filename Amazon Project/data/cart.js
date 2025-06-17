5558f0f2ee94d0952f7cdd247158a03b2e72f0cc
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-17 19:11:59 +0700

    save cart in localStorage

--- Amazon Project/data/cart.js
+++ Amazon Project/data/cart.js
@@ -1,10 +1,21 @@
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}]; // agar variablenya bisa dipake diluar file cart.js
+// export agar variablenya bisa dipake diluar file cart.js
+export let cart = JSON.parse(localStorage.getItem('cart')); 
+// step 2 about local storage ==> balikin ke tipe data awal, dengan JSON.parse
+
+if(!cart){
+  cart = [{
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2
+          }, {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1
+          }
+        ]; 
+}
+
+function saveToStorage(){ // step 1 about local storage ==> taro nama local storage (bebas)
+  localStorage.setItem('cart', JSON.stringify(cart)); // harus di stringify karena local storage cuma bisa save strings
+}
 
 export function addToCart(productId){
   let matchingItem;
@@ -22,6 +33,8 @@ export function addToCart(productId){
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -34,4 +47,6 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+  
+  saveToStorage();
 }
