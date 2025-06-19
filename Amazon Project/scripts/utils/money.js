dd2f038bfd67d18bfe3b3ee0a9edd105f726ab9f
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-19 19:59:59 +0700

    save data and generate HTML for delivery options

--- Amazon Project/scripts/utils/money.js
+++ Amazon Project/scripts/utils/money.js
@@ -1,3 +1,6 @@
 export function formatCurrency(priceCents){
   return (priceCents / 100).toFixed(2);
 }
+
+export default formatCurrency;
+// export default dipake kalo hanya ada 1 function yg mau diexport keluar file
