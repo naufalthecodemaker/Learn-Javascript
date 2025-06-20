6e2687ea7f7139a3f3742a78ef98913d89bb2c90
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-20 21:03:58 +0700

    create automated tests for formatCurrency

---
+++ Amazon Project/tests/moneyTest.js
@@ -0,0 +1,28 @@
+// melakukan automated test
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite: formatCurrenc function');
+
+// basic test cases
+console.log('converts cents into dollars:');
+
+if (formatCurrency(2095) === '20.95'){
+  console.log('passed');
+} else{
+  console.log('failed');
+}
+
+// edge test cases
+console.log('works with 0:');
+if(formatCurrency(0) === '0.00'){
+  console.log('passed');
+} else{
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent:');
+if(formatCurrency(2000.5) === '20.01'){
+  console.log('passed');
+} else{
+  console.log('failed');
+}
