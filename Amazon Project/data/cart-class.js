e587df1b7d5fa1418370b253b35811645eb0b880
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-30 18:49:40 +0700

    make localStorageKey and loadFromStorage private

--- Amazon Project/data/cart-class.js
+++ Amazon Project/data/cart-class.js
@@ -2,18 +2,18 @@
 class Cart {
   // add properties
   cartItems; // gaperlu pake undefined
-  localStorageKey;
+  #localStorageKey; // agar menjadi private property (hanya bisa digunakan di class ini)
   validDeliveryOption = './deliveryOptions.js';
 
   // add methods
   constructor(localStorageKey){
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
   // shorcut dari loadFromStorage: function(){}
-  loadFromStorage(){
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)); 
+  #loadFromStorage(){ // private method
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)); 
   
     if(!this.cartItems){
       this.cartItems = [{
@@ -29,7 +29,7 @@ class Cart {
   }
 
   saveToStorage(){ 
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId){
