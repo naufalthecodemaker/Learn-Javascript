ec51d0064dcd3fd1363a1e984a1f6eb251b97cfd
naufalthecodemaker <naufalrahman867@gmail.com>
2025-06-30 18:37:53 +0700

    create class version of cart

--- Amazon Project/scripts/checkout/paymentSummary.js
+++ Amazon Project/scripts/checkout/paymentSummary.js
@@ -15,7 +15,7 @@ export function calculateCartQuantity(){
 export function renderPaymentSummary(){
   let productPriceCents = 0;
   let shippingPriceCents = 0;
-  cart.forEach((cartItem) => {
+  cart.forEach((cartItem) => {  
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
   
