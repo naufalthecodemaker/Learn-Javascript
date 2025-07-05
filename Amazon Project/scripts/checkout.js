fbf3548e719e8d2edfa014f3468e9124f4bd664c
naufalthecodemaker <naufalrahman867@gmail.com>
2025-07-05 10:05:53 +0700

    learn promises and convert code to promises

--- Amazon Project/scripts/checkout.js
+++ Amazon Project/scripts/checkout.js
@@ -2,12 +2,59 @@ import {renderCheckoutHeader} from "./checkout/checkoutHeader.js";
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {loadProducts} from "../data/products.js";
+import {loadCart} from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/car.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1'); // to go to next step after finishing running the asysc code
+    });
+  }),
+  new Promise((resolve) => { 
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1'); // to go to next step after finishing running the asysc code
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => { 
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/* tanpa promises ==> nested callbacks
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
